ae26fa7582d65002b105bc245e56c4f6b92fc34f
rosemarymagoma99@gmail.com <rosemarymagoma99@gmail.com>
2018-09-26 09:22:59 +0300

    add grids to the html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,23 @@
 //Busines logic
-var answeredAll = 0;
-var questions = function(Qn1,Qn2, Qn3, Qn4, Qn5){
-  return Qn1 + Qn2 + Qn3 + Qn4 + Qn5
-    answeredAll+=1
 
-};// Business logic
+var myQuestions = function(questionOne, questionTwo, questionThree, questionFour, questionFive){
+  return questionOne + questionTwo + questionThree + questionFour + questionFive
+
+
+};//  End of Business logic
 //User interface
 $(document).ready(function(){
 $("form#test").submit(function(event){
   event.preventDefault();
-var Qn1 = parseInt($("input:radio[name=choices]:checked").val());
-var Qn2 = parseInt($("input:radio[name=answer]:checked").val());
-var Qn3 = parseInt($("input:radio[name=answer1]:checked").val());
-var Qn4 = parseInt($("input:radio[name=answer2]:checked").val());
-var Qn5 = parseInt($("input:radio[name=answer3]:checked").val());
+var questionOne = parseInt($("input:radio[name=choices]:checked").val());
+var questionTwo = parseInt($("input:radio[name=answer]:checked").val());
+var questionThree = parseInt($("input:radio[name=answer1]:checked").val());
+var questionFour = parseInt($("input:radio[name=answer2]:checked").val());
+var questionFive = parseInt($("input:radio[name=answer3]:checked").val());
 
 
- var total = questions(Qn1, Qn2, Qn3, Qn4, Qn5);
+ var totalScore = myQuestions(questionOne, questionTwo, questionThree, questionFour, questionFive);
  $(".development").hide();
- $("#outputs").text(total);
- var remarks = "";
- var bad = 10;
- var average = 30;
- var good = 40;
- var perfect =50;
-
- if (answeredAll===5) {
-   report = "you have scored:";
-
-   if (score <= 10) {
-     $("#remark").text("bad");
-   }else if(score <= 30) {
-   $("#remark").text("average");
- }else if (score <= 40) {
-   $("#remark").text("good");
- } else if(score === 50){
-   $("#remark").text("Perfect");
- } else {
-   report = "Ensure that you answer all questions and resubmit."
- }
- }
- var remarks = (bad,average,good,perfect);
- $("#remark").text
-
+ $("#outputs").text(totalScore);
 });
 });
